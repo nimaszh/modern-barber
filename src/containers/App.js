7167e9c2dfe0fc1357af45c95dd82cf761528868
nimaszh <76870269+nimaszh@users.noreply.github.com>
2022-11-12 15:49:31 +0100

    ressting

--- src/containers/App.js
+++ src/containers/App.js
@@ -67,7 +67,7 @@ function App() {
             function ReserveDates() {
                 const [data,setData] = useState(null);
                 useEffect(() => {
-                    fetch('http://localhost:3000/masoodtable', {
+                    fetch('http://141.11.42.106:3000/masoodtable', {
                       method: 'get',
                       headers: {'Content-Type': 'application/json'},
                     }).then(res => res.json())
